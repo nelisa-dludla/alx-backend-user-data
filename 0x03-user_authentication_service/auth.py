db502331f6e5260eb47d52aec9b79b4ef444cacc
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-12 19:25:00 +0200

    Updated auth.py - Refactored register_user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import uuid
 from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -26,21 +27,19 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         '''Registers the User
         '''
-        result = self._db._session.query(User).filter_by(email=email).first()
-        if result:
+        try:
+            user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
 
-        hashed_password = _hash_password(password)
-        user = User(email=email, hashed_password=hashed_password)
-        self._db._session.add(user)
-        self._db._session.commit()
-
-        return user
+            return user
 
     def valid_login(self, email: str, password: str) -> bool:
         '''Checks if the user is valid
         '''
-        user = self._db._session.query(User).filter_by(email=email).first()
+        user = self._db.find_user_by(email=email)
         if user:
             return checkpw(password.encode('utf-8'), user.hashed_password)
 
