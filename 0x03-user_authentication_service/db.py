af5c7ecdc358c1fd9a11579fed58fb9a27292aff
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-12 16:06:54 +0200

    Updated db.py - Updated add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,11 +37,12 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         '''Add a new user to DB
         '''
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        if email and hashed_password:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
 
-        return user
+            return user
 
     def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
         '''Find user by kwargs
